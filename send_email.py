d1951e56f10c6c9f9989c2e3603fa6d0666630e2
Ankit Velani <ankit.velani@gmail.com>
2023-03-20 15:15:20 +0530

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -6,6 +6,11 @@ port = 465
 smtp_server = "smtp.gmail.com"
 USERNAME = os.environ.get('MAIL_USERNAME')
 PASSWORD = os.environ.get('MAIL_PASSWORD')
+
+sender =     'ankit.velani@gmail.com'
+destination = ['ankit.velani@gmail.com', 'velaniankits@gmail.com']
+
+
 message = """\
 Subject: GitHub Email Report
 This is your daily email report.
@@ -14,4 +19,4 @@ This is your daily email report.
 context = ssl.create_default_context()
 with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
     server.login(USERNAME,PASSWORD)
-    server.sendmail(USERNAME,USERNAME,message)
+    server.sendmail(sender,destination,message)
