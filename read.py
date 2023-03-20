520f7d20cf4d52c45d9e0951616f2e08c61e4eda
Ankit Velani <ankit.velani@gmail.com>
2023-03-20 15:46:40 +0530

    Update read.py

--- read.py
+++ read.py
@@ -1,4 +1,6 @@
-file_name = os.environ.get('LOG_FILE')
-with open(file_name, encoding='utf8') as f:
-    for line in f:
-        print(line.strip())
+# file_name = os.environ.get('LOG_FILE')
+# with open(file_name, encoding='utf8') as f:
+#     for line in f:
+#         print(line.strip())
+
+print("Hello World!!!")
