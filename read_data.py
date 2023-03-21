90786741c0841f5e10ead10973578ceadfb438e8
Ankit Velani <ankit.velani@gmail.com>
2023-03-21 06:04:51 +0530

    Update read_data.py

--- read_data.py
+++ read_data.py
@@ -3,7 +3,7 @@ import os
 
 arr = os.listdir()
 print(arr)
-# file_name = os.environ.get('LOG_FILE')
-# with open(file_name, encoding='utf8') as f:
-#     for line in f:
-#         print(line.strip())
+file_name = "sample.log"
+with open(file_name, encoding='utf8') as f:
+    for line in f:
+        print(line.strip())
