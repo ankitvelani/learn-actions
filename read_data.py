d7d311666ef003e1583378cb92ee732a6c3c9dbb
Ankit Velani <ankit.velani@gmail.com>
2023-03-23 15:45:22 +0530

    Update read_data.py

--- read_data.py
+++ read_data.py
@@ -7,3 +7,7 @@ file_name = "sample.log"
 with open(file_name, encoding='utf8') as f:
     for line in f:
         print(line.strip())
+
+
+with open('file1.txt', 'w') as f:
+    print('hello world', file=f)
